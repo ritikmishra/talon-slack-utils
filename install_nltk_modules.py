163ec1398629e11653f3e62817af52ac1ae9f891
Ritik Mishra <ritik.mishra314@gmail.com>
2017-05-02 23:24:57 -0500

    travis don't fail me

--- install_nltk_modules.py
+++ install_nltk_modules.py
@@ -1,7 +1,11 @@
 """Download all the necessary modules for the server."""
 import nltk
-if __name__ == "__main__":
+
+def install():
     modules = open("nltk.txt", "r")
     for corpus in modules:
         print("Installing " + corpus + ". . .")
         nltk.download(corpus)
+
+if __name__ == "__main__":
+    install()
