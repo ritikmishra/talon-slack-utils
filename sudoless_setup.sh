eeff104f98d117ec48ab02d7af52b21d924ad760
Ritik Mishra <ritik.mishra314@gmail.com>
2017-05-03 21:23:01 -0500

    fixed sudoless setup. the issue was that pip needed to install the wheel package to some directory in /Library, but easy_install will be nice and install it to ~/python_packages

--- sudoless_setup.sh
+++ sudoless_setup.sh
@@ -8,8 +8,11 @@ mkdir ~/python_packages
 
 echo "export PATH=\"$HOME/python_packages:$HOME/node/node-v6.10.3-darwin-x64/bin:$PATH\"\nexport PYTHONPATH=$HOME/python_packages" >> ~/.bash_profile
 
-source ~/.bash_profile
+export PYTHONPATH=$HOME/python_packages
 
 easy_install --install-dir=$HOME/python_packages pip
+easy_install --install-dir=$HOME/python_packages wheel
 
 pip install -r requirements.txt
+
+source ~/.bash_profile
