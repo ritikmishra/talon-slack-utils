b75608068b4a22a746036ff811398ccee33dd05e
Ritik Mishra <ritik.mishra314@gmail.com>
2017-10-07 07:52:07 -0500

    thinker test fixed

---
+++ conjugate.js
@@ -0,0 +1,10 @@
+"use strict";
+let nlp = require("nlp_compromise");
+var args = process.argv.slice(2);
+var verb = args[0]
+var tense = args[1]
+
+
+//should go like
+//node conjugate.js [verb] [tense] [noun type]
+console.log(nlp.verb(verb).conjugate()[tense])
